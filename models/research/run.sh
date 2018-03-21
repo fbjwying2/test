f6225e9d471cd4e83d55f94a1a6b06c4119785d7
HataFeng <fbjwying2@163.com>
2018-03-21 10:54:36 +0800

    inference

--- models/research/run.sh
+++ models/research/run.sh
@@ -27,7 +27,7 @@ pipeline_config_path=$output_dir/$config
 # 因为dataset里面的东西是不允许修改的，所以这里要把config文件复制一份到输出目录
 cp $DIR/object_detection/samples/configs/$config $pipeline_config_path
 
-for i in {0..15}  # for循环中的代码执行5此，这里的左右边界都包含，也就是一共训练500个step，每100step验证一次
+for i in {0..0}  # for循环中的代码执行5此，这里的左右边界都包含，也就是一共训练500个step，每100step验证一次
 do
     echo "############" $i "runnning #################"
     last=$[$i*100]
@@ -42,7 +42,7 @@ do
 done
 
 # 导出模型
-python ./object_detection/export_inference_graph.py --input_type image_tensor --pipeline_config_path $pipeline_config_path --trained_checkpoint_prefix $train_dir/model.ckpt-$current  --output_directory $output_dir/exported_graphs
+# python ./object_detection/export_inference_graph.py --input_type image_tensor --pipeline_config_path $pipeline_config_path --trained_checkpoint_prefix $train_dir/model.ckpt-$current  --output_directory $output_dir/exported_graphs
 
 # 在test.jpg上验证导出的模型
-python ./object_detection/inference.py --output_dir=$output_dir --dataset_dir=$dataset_dir
+python ./object_detection/inference.py --output_dir=$output_dir --dataset_dir=$DIR
